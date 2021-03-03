fda326dc2614ffa78099d325d26d1a8992797b39
Yuvraj Sharma <yuvrajtest@outlook.com>
2021-03-03 18:32:16 +0530

    Removed unnecessary routes
    > Removed unnecessary pages and routes to simplify the frontend

--- router/customer.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = new express.Router()
-const auth = require('middleware/authentication')
-const User = require('model/farmer') /*Loading user model */
-
-router.get('/customer', auth, async (req, res) => {
-    res.render('customer')
-})
-module.exports = router
